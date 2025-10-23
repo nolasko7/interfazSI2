9b2d26b0395d9bc7d7d8202b46a386ec8d81f78f
nolasko7 <valentin.nolasco123123@gmail.com>
2025-10-23 18:10:33 -0300

    algunas implementaciones realizadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <nav className = 'navBar'>
+      <button className = 'boton'> Suscribirse </button>
+      <button className = 'boton'> Iniciar Sesion </button>
+    </nav>
+    
+    <div>
+      <div className = 'contenedor-lista'>
+        <button className = 'boton-lista izquierda'>
+           &lt;
+        </button>
+        <button className = 'boton-lista derecha'>
+           &gt;
+        </button>
+        <div className = 'listas'>
+          <a href = 'https://www.google.com'>
+            <img className = 'lista-imagen' src = 'src\assets\pelicula.jpg' alt = 'barbie la pelicula'></img>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
 )
